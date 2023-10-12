2325a965a266cef66bb7b26c0a484852d8925976
AudioExtreme <146911721+AudioExtreme@users.noreply.github.com>
2023-10-11 22:44:53 -0600

    Update sign-up.js

--- src/pages/join/sign-up/sign-up.js
+++ src/pages/join/sign-up/sign-up.js
@@ -3,12 +3,26 @@ import { JoinLayout } from "@/layouts";
 import { RegisterForm } from "@/components/Auth";
 import { Seo } from "@/components/Shared";
 import styles from "./sign-up.module.scss";
+import Head from 'next/head';
 
 export default function SignUpPage() {
   return (
     <>
 
       <Seo title="Registrarse"/>
+
+    <Head>
+        {/* Agrega tus etiquetas de Google Tag Manager aquí */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-42911K2147"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-42911K2147');
+          `}
+        </script>
+      </Head>
 
       <JoinLayout>
         <div className={styles.signIn}>
