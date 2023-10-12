cfa954abc8c63bd58ebb9aa4b97147b7437481fe
AudioExtreme <146911721+AudioExtreme@users.noreply.github.com>
2023-10-11 22:09:26 -0600

    Home Analitics

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -17,6 +17,16 @@ export default function HomePage() {
      
      <Seo/>
 
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-42911K2147"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-42911K2147');
+</script>
+
       <BasicLayout>
         
         <Home.BannerLastProductoPublished 
