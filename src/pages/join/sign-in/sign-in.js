00b0b06a1e4ef2820446ff854fe3fc51e53f48fa
AudioExtreme <146911721+AudioExtreme@users.noreply.github.com>
2023-10-11 22:44:25 -0600

    Update sign-in.js

--- src/pages/join/sign-in/sign-in.js
+++ src/pages/join/sign-in/sign-in.js
@@ -3,12 +3,26 @@ import { JoinLayout } from "@/layouts";
 import { LoginForm } from "@/components/Auth";
 import { Seo } from "@/components/Shared";
 import styles from "./sign-in.module.scss";
+import Head from 'next/head';
 
 export default function SignInPage() {
   return (
     <>
       
       <Seo title="Iniciar sesión"/>
+
+    <Head>
+        {/* Agrega tus etiquetas de Google Tag Manager aquí */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-42911K2147"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-42911K2147');
+          `}
+        </script>
+      </Head>
 
       <JoinLayout>
         <div className={styles.signIn}>
