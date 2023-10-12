51e0075ac6e5b37c06b1020d704dfa80309e17cd
AudioExtreme <146911721+AudioExtreme@users.noreply.github.com>
2023-10-11 22:46:27 -0600

    Update producto.js

--- src/pages/[producto]/producto.js
+++ src/pages/[producto]/producto.js
@@ -1,6 +1,7 @@
 import { BasicLayout } from "@/layouts";
 import { Producto } from "@/components/Producto";
 import { Separator, Seo } from "@/components/Shared";
+import Head from 'next/head';
 
 export default function ProductoPage(props) {
   const { producto } = props;
@@ -12,6 +13,19 @@ export default function ProductoPage(props) {
         title={producto.attributes.title}
         description={producto.attributes.summary}
       />
+
+        <Head>
+        {/* Agrega tus etiquetas de Google Tag Manager aquí */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-42911K2147"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-42911K2147');
+          `}
+        </script>
+      </Head>
 
       <BasicLayout>
         <Producto.HeaderWallpaper image={wallpaper.data.attributes.url} />
